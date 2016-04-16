84c965d599c1ce5f0ae520dc00912c645eac8699
amarraja123 <amar.arawat@gmail.com>
2016-04-17 00:30:17 +0530

    basics

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,11 @@
 import {Component} from 'angular2/core';
+import {CoursesComponents} from "./courses.component";
+import {AuthorsComponents} from "./authors.components";
 
 @Component({
 	selector: 'my-app',
-	template: '<h1>My SECOND Angular 2 App</h1>'
+	template: '<h1>My SECOND Angular 2 App</h1><courses></courses><authors></authors>',
+	directives:[CoursesComponents, AuthorsComponents]
 })
 
 export class AppComponent { }
