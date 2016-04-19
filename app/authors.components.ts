1a2487c0e63bd7d940d5086f659caceca8c2ba25
amarraja123 <amar.arawat@gmail.com>
2016-04-19 16:59:34 +0530

    basics

--- app/authors.components.ts
+++ app/authors.components.ts
@@ -3,15 +3,7 @@ import {Component} from "angular2/core";
 import {AuthorService} from "./author.service";
 @Component({
     selector:'authors',
-    template: `
-            <h2>Authors</h2>
-            {{title}}
-            <ul>
-                <li *ngFor="#author of authors">
-                    {{author}}
-                </li>
-            </ul>
-            `,
+    templateUrl: './app/authors.template.html',
     providers:[AuthorService]
 })
 
